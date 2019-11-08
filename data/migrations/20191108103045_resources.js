093e2d2ae751449806e6ae60505d35b1f739ca5d
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-08 12:46:08 -0600

    resource name now unique

--- data/migrations/20191108103045_resources.js
+++ data/migrations/20191108103045_resources.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('resources', tbl => {
         tbl.increments();
-        tbl.string('name', 255).notNullable();
+        tbl.string('name', 255).unique().notNullable();
         tbl.string('description', 255);
     })
 
