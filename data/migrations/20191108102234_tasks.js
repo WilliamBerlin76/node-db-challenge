f8052e2c209c0771baa83d0beba93fee1dc90c18
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-08 10:49:05 -0600

    migrations created

---
+++ data/migrations/20191108102234_tasks.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('description', 225).notNullable();
+        tbl.string('notes', 225);
+        tbl.boolean('completed').notNullable().defaultTo(false)
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema  
+    .dropTableIfExists('tasks')
+};
