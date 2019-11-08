d5bef4644581bed3994794b931c60b4692abf4c5
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-08 12:19:52 -0600

    mvp for project portion complete, need to answer questions

--- projects/project-model.js
+++ projects/project-model.js
@@ -1,9 +1,41 @@
 const db = require('../data/db-config.js');
 
 module.exports = {
-    find
+    getProjects,
+    addProject,
+    getResources,
+    addResource,
+    getTasks,
+    addTask
 };
 
-function find(){
-    
+function getProjects(){
+    return db('projects');
+}
+
+function addProject(body){
+    return db('projects')
+            .insert(body)
+};
+
+function getResources(){
+    return db('resources');
+};
+
+function addResource(body){
+    return db('resources')
+            .insert(body)
+};
+
+function getTasks(project_id){
+    return db.select('projects.Name', 'projects.description', 'tasks.description', 'tasks.notes', 'tasks.completed')
+            .where('projects.id', '=', project_id)
+            .from('projects')
+            .join('tasks', 'tasks.project_id', '=', 'projects.id')
+};
+
+function addTask(task, project_id){
+    return db('tasks')
+            .insert(task)
+            .where('project.id', '=', project_id)
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -2,8 +2,89 @@ const express = require('express');
 
 const Projects = require('./project-model');
 
-const router = express.Router
+const router = express.Router();
 
+router.get('/projects', (req, res) => {
+    Projects.getProjects()
+        .then(projects => {
+            arr = [];
+            projects.map(item => {
+                if (item.completed === 0 || item.completed === null){
+                    arr.push({...item, completed: false})
+                } else {
+                    arr.push({...item, completed: true})
+                }
+            })
+            res.status(200).json(arr)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'could not retrieve the projects'})
+        })
+});
 
+router.post('/projects', (req, res) => {
+    const body = req.body;
+    Projects.addProject(body)
+    .then(project => {
+        res.status(200).json(project)
+    })
+});
+
+router.get('/resources', (req, res) => {
+    Projects.getResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'could not retrieve resources'})
+        })
+});
+
+router.post('/resources', (req, res) => {
+    const body = req.body;
+    Projects.addResource(body)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'could not add the resource'})
+        })
+});
+
+router.get('/projects/:id/tasks', (req, res) => {
+    const id = req.params.id;
+    Projects.getTasks(id)
+        .then(tasks => {
+            let arr = []
+            tasks.map(task => {
+                if (task.completed === 0 || task.completed === null){
+                    return arr.push({...task, completed: false})
+                } else {
+                    return arr.push({...task, completed: true})
+                }
+            })
+            res.status(200).json(arr)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({error: 'could not retrieve the tasks'})
+        })
+});
+
+router.post('/projects/:id/tasks', (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    Projects.addTask(body, id)
+        .then(task => {
+            res.status(200).json(task)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'could not add task to the project'})
+        })
+});
 
 module.exports = router;
